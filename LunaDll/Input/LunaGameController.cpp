1fc834e39f96d82c8547e8ebfc162fef92bb708a
Spencer Everly <spencer.everly@gmail.com>
2023-08-16 15:17:13 -0500

    Forgot this

--- LunaDll/Input/LunaGameController.cpp
+++ LunaDll/Input/LunaGameController.cpp
@@ -889,6 +889,15 @@ void LunaGameController::controllerButtonEvent(const SDL_ControllerButtonEvent& 
     default:
         break;
     }
+    
+#if !defined(BUILDING_SMBXLAUNCHER)
+    if (gLunaLua.isValid()) {
+        std::shared_ptr<Event> controllerEventButton = std::make_shared<Event>("onControllerButtonEvent", false);
+        controllerEventButton->setDirectEventName("onControllerButtonEvent");
+        controllerEventButton->setLoopable(false);
+        gLunaLua.callEvent(controllerEventButton, event.button, down);
+    }
+#endif
 }
 
 
