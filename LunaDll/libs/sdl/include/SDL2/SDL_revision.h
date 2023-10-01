888f5b2836db88a1a24e1d28137f40affd407afd
Spencer Everly <spencer.everly@gmail.com>
2023-10-01 13:38:09 -0500

    Seeing if this'll let the merge go through

--- LunaDll/libs/sdl/include/SDL2/SDL_revision.h
+++ LunaDll/libs/sdl/include/SDL2/SDL_revision.h
@@ -1,8 +1,1 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
 
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2023-04-15-33-g99be6983 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-2023-04-15-33-g99be6983"
-#endif
