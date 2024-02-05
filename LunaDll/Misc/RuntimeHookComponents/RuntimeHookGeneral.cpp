37d5c2fb303fc151421ee611adc66a502cd70ab4
Spencer Everly <spencer.everly@gmail.com>
2024-02-04 21:42:41 -0600

    Forgot to add 1 to keep it consistent with Lua

--- LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
+++ LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -418,11 +418,11 @@ static void ProcessRawKeyPress(uint32_t virtKey, uint32_t scanCode, bool repeate
         if (unicodeRet > 0)
         {
             std::string charStr = WStr2Str(std::wstring(unicodeData, unicodeRet));
-            gLunaLua.callEvent(keyboardPressEvent, static_cast<int>(virtKey), repeated, charStr, keyboardIdx);
+            gLunaLua.callEvent(keyboardPressEvent, static_cast<int>(virtKey), repeated, charStr, keyboardIdx + 1);
         }
         else
         {
-            gLunaLua.callEvent(keyboardPressEvent, static_cast<int>(virtKey), repeated, "", keyboardIdx);
+            gLunaLua.callEvent(keyboardPressEvent, static_cast<int>(virtKey), repeated, "", keyboardIdx + 1);
         }
     }
 
